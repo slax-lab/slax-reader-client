ee33235c8bba7793ef84f07454217ddd0c773f61
the-eric-kwok <the-eric-kwok@users.noreply.github.com>
2025-10-23 15:00:40 +0800

    :art: feat: implement KVO on iOS

---
+++ composeApp/src/iosMain/kotlin/com/slax/reader/utils/KeyValueObserver.kt
@@ -0,0 +1,46 @@
+package com.slax.reader.utils
+
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSKeyValueChangeNewKey
+import platform.Foundation.NSKeyValueChangeOldKey
+import platform.Foundation.NSKeyValueObservingOptionInitial
+import platform.Foundation.NSKeyValueObservingOptionNew
+import platform.Foundation.NSKeyValueObservingOptionOld
+import platform.Foundation.addObserver
+import platform.Foundation.removeObserver
+import platform.darwin.NSObject
+import platform.foundation.NSKeyValueObservingProtocol
+
+@OptIn(ExperimentalForeignApi::class)
+class KVOObserver(
+    private val onValueChanged: (keyPath: String, oldValue: Any?, newValue: Any?) -> Unit
+) : NSObject(), NSKeyValueObservingProtocol {
+
+    @OptIn(ExperimentalForeignApi::class)
+    override fun observeValueForKeyPath(
+        keyPath: String?,
+        ofObject: Any?,
+        change: Map<Any?, *>?,
+        context: COpaquePointer?
+    ) {
+        keyPath?.let { path ->
+            val newValue = change?.get(NSKeyValueChangeNewKey)
+            val oldValue = change?.get(NSKeyValueChangeOldKey)
+            onValueChanged(path, newValue, oldValue)
+        }
+    }
+
+    fun observe(keyPath: String, ofObject: NSObject) {
+        ofObject.addObserver(
+            observer = this,
+            forKeyPath = keyPath,
+            options = NSKeyValueObservingOptionNew or NSKeyValueObservingOptionOld or NSKeyValueObservingOptionInitial,
+            context = null
+        )
+    }
+
+    fun unobserve(keyPath: String, ofObject: NSObject) {
+        ofObject.removeObserver(observer = this, forKeyPath = keyPath)
+    }
+}
