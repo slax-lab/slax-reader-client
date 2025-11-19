d334d7011cf18d054d962be2e0ec4056807ef868
daguang <daguang830@gmail.com>
2025-11-19 11:03:24 +0800

    :bug: fix local bookmark download status not change and remove download status

--- composeApp/src/commonMain/kotlin/com/slax/reader/ui/inbox/compenents/BookmarkItemStatus.kt
+++ composeApp/src/commonMain/kotlin/com/slax/reader/ui/inbox/compenents/BookmarkItemStatus.kt
@@ -6,12 +6,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,9 +23,11 @@ fun ItemStatus(bookmarkId: String, viewModel: InboxListViewModel) {
     val iconResource = painterResource(Res.drawable.ic_cell_internet)
     val iconPainter = remember { iconResource }
 
-    val downloadStatus by remember {
+    val localBookmarkMap by viewModel.localBookmarkMap.collectAsState()
+    val downloadStatus by remember(bookmarkId) {
         derivedStateOf {
-            viewModel.localBookmarkMap.value[bookmarkId]?.downloadStatus
+            val status = localBookmarkMap[bookmarkId]?.downloadStatus
+            if (status == null || status != 2) null else 2
         }
     }
 
@@ -39,14 +37,11 @@ fun ItemStatus(bookmarkId: String, viewModel: InboxListViewModel) {
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        val isDownloading = downloadStatus == 1
-        val isCompleted = downloadStatus == 2
-
         Surface(
             modifier = Modifier.size(18.dp),
             shape = RoundedCornerShape(50),
             color = Color(0xFFF5F5F3),
-            border = BorderStroke(0.5.dp, if (isCompleted) Color(0xFFC4C4C2) else Color.Transparent)
+            border = BorderStroke(0.5.dp, if (downloadStatus == 2) Color(0xFFC4C4C2) else Color.Transparent)
         ) {}
 
         Image(
@@ -55,14 +50,5 @@ fun ItemStatus(bookmarkId: String, viewModel: InboxListViewModel) {
             modifier = Modifier.size(12.dp),
             contentScale = ContentScale.Fit
         )
-
-        if (isDownloading) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(18.dp),
-                color = Color(0xFF1DA1F2),
-                strokeWidth = 2.dp,
-                trackColor = Color.Transparent
-            )
-        }
     }
 }
