090bc577622f85e11eaa241023fb10590bad0c46
YaoJunchang <yaojunchang@zsxq.com>
2026-01-22 15:26:42 +0800

    :art: add feedback import

--- react-native/index.js
+++ react-native/index.js
@@ -1,6 +1,8 @@
 import {AppRegistry} from 'react-native';
 import MarkdownPage from './src/Markdown';
-import ChatPage from "./src/Chat";
+import ChatPage from './src/Chat';
+import FeedbackPage from './src/Feedback';
 
 AppRegistry.registerComponent('RNMarkdownPage', () => MarkdownPage);
 AppRegistry.registerComponent('RNChatPage', () => ChatPage);
+AppRegistry.registerComponent('RNFeedbackPage', () => FeedbackPage);
