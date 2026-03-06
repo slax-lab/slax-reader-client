dfaeb1b9ed240da45c097bd9ca2801b1a9da9c1b
YaoJunchang <yaojunchang@zsxq.com>
2026-03-06 11:16:19 +0800

    🎨 trigger PageLoaded event on "domReady" message instead of onPageFinished

--- composeApp/src/androidMain/kotlin/com/slax/reader/utils/WebviewHelper.android.kt
+++ composeApp/src/androidMain/kotlin/com/slax/reader/utils/WebviewHelper.android.kt
@@ -85,6 +85,10 @@ actual fun AppWebView(
                         runCatching { Json.decodeFromString<WebViewMessage>(message) }
                             .onSuccess { msg ->
                                 when (msg.type) {
+                                    "domReady" -> {
+                                        webState.dispatchEvent(WebViewEvent.PageLoaded)
+                                    }
+
                                     "imageClick" -> {
                                         webState.dispatchEvent(
                                             WebViewEvent.ImageClick(msg.src!!, msg.allImages!!)
@@ -129,7 +133,6 @@ actual fun AppWebView(
 
                     override fun onPageFinished(view: WebView?, url: String?) {
                         super.onPageFinished(view, url)
-                        webState.dispatchEvent(WebViewEvent.PageLoaded)
                     }
                 }
 
