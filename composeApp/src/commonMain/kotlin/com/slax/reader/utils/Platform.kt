2b2a013e3535d704b08158e49e3420702a7ff018
daguang <28806852+DGuang21@users.noreply.github.com>
2026-04-29 15:07:34 +0800

    🎨 add http client request UA (#105)

--- composeApp/src/commonMain/kotlin/com/slax/reader/utils/Platform.kt
+++ composeApp/src/commonMain/kotlin/com/slax/reader/utils/Platform.kt
@@ -2,5 +2,7 @@ package com.slax.reader.utils
 
 expect val platformType: String
 
+expect val platformName: String
+
 fun isAndroid(): Boolean = platformType == "android"
 fun isIOS(): Boolean = platformType == "ios"
