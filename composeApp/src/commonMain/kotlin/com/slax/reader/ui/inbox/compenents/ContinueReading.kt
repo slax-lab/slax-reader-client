2633357b2da2be526e22cda8c2374a933c89b075
YaoJunchang <yaojunchang@zsxq.com>
2026-08-18 14:44:02 +0800

    🐛 prevent blank continue reading titles

--- composeApp/src/commonMain/kotlin/com/slax/reader/ui/inbox/compenents/ContinueReading.kt
+++ composeApp/src/commonMain/kotlin/com/slax/reader/ui/inbox/compenents/ContinueReading.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.slax.reader.data.preferences.AppPreferences
 import com.slax.reader.data.preferences.ContinueReadingBookmark
+import com.slax.reader.utils.collapseWhitespace
 import com.slax.reader.utils.i18n
 import com.slax.reader.utils.FirstPartyEventReporter
 import org.jetbrains.compose.resources.painterResource
@@ -45,8 +46,10 @@ fun ContinueReading(
 
     LaunchedEffect(Unit) {
         val bookmark = appPreferences.getContinueReadingBookmark() ?: return@LaunchedEffect
-        showContinueData = bookmark
         appPreferences.clearContinueReadingBookmark()
+        val normalizedTitle = bookmark.title.collapseWhitespace()
+        if (normalizedTitle.isBlank()) return@LaunchedEffect
+        showContinueData = bookmark.copy(title = normalizedTitle)
         dismissed = false
         visibilityState.targetState = true
     }
